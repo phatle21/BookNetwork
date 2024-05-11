ce1878214b8c20f7096036ddcd778ff9f6be67e0
phatle21 <patrick.le.204@gmail.com>
2024-05-11 10:05:12 +0700

    done mini-project

---
+++ book-network/src/main/java/com/phat/book/history/BookTransactionHistory.java
@@ -0,0 +1,32 @@
+package com.phat.book.history;
+
+import com.phat.book.book.Book;
+import com.phat.book.common.BaseEntity;
+import com.phat.book.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
